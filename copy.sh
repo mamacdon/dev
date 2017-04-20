25c9fb13bf8e8b0f4e1dcfba99a1d036822c4217
Mark Macdonald <mamacdon@ca.ibm.com>
2017-04-20 14:12:06 -0400

    Add $HOME/.gitconfig

--- copy.sh
+++ copy.sh
@@ -3,7 +3,7 @@ DEV=$HOME/code/misc/dev
 BIN="$HOME/bin"
 SUBLIME_USER="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 
-HOME_SCRIPTS=(".bash_aliases")
+HOME_SCRIPTS=('.bash_aliases' '.gitconfig')
 BIN_SCRIPTS=('go_development.sh' 'git-link.sh' 'home.mamacdon.ca.sh' 'lock_screen.sh' 'nodejs_helpers.sh')
 SUBLIME_PREFS=('Default (OSX).sublime-keymap' 'Preferences.sublime-settings' 'SublimeLinter.sublime-settings')
 
